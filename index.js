52105fa559d2ce2c8321306c3c96e0114913e92c
Anne Golladay <annegolladay@gmail.com>
2021-03-25 16:55:14 -0600

    task 6

--- index.js
+++ index.js
@@ -215,8 +215,18 @@ console.log('Task 5:', michael.listSubjects());
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(attrs) {
+      super(attrs);
+      this.gradClassName = attrs.gradClassName;
+      this.favInstructor = attrs.favInstructor;
+     }
+     standUp(slackChannel) {
+       return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+     }
+     debugsCode(student, subject) {
+       return `${this.name} debugs ${student.name}'s code on ${subject}`;
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
